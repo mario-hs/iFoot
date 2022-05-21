fe7e0f8215ee7e99bfc7f3ec520ca733cee45625
vicenzo <vicenzo.alp@gmail.com>
2022-05-21 11:01:44 -0300

    export java arquivos trabalho banco

---
+++ demo/src/main/java/ifoot/demo/Campo.java
@@ -0,0 +1,17 @@
+package java;
+
+public class Campo extends ReservaGrupo {
+
+	private Integer id;
+
+	private String nomeCampo;
+
+	private Float valorUnit;
+
+	private Float valorMes;
+
+	private CampoHorario[] campoHorario;
+
+	private Espaco espaco;
+
+}
