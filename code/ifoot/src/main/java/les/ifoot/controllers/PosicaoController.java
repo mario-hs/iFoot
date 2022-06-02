b6be55e7f6c18b888ee1daac53765001645ee6d7
Mario Henrique <mario-hp41@hotmail.com>
2022-06-02 15:58:52 -0300

    refatoração do codigo

--- code/ifoot/src/main/java/les/ifoot/controllers/PosicaoController.java
+++ code/ifoot/src/main/java/les/ifoot/controllers/PosicaoController.java
@@ -1,6 +1,7 @@
 package les.ifoot.controllers;
 
 import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import les.ifoot.model.Posicao;
 import les.ifoot.services.PosicaoService;
 
