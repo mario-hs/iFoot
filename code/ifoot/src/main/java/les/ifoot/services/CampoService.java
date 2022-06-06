dc54c2f6937fae02e5e6d1580a920498781aea5b
vicenzo <vicenzo.alp@gmail.com>
2022-06-06 20:44:42 -0300

    cadastro campo concluido e com post dando erro de forma certa

--- code/ifoot/src/main/java/les/ifoot/services/CampoService.java
+++ code/ifoot/src/main/java/les/ifoot/services/CampoService.java
@@ -1,35 +1,84 @@
 package les.ifoot.services;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
 import les.ifoot.model.Campo;
 import les.ifoot.repositories.CampoRepository;
+import les.ifoot.services.exceptions.DataIntegrityException;
 
 @Service
 public class CampoService {
+    // @Autowired
+    // private CampoRepository repository;
+
+    // public Campo findById(Integer id) {
+    //     return repository.findById(id).get();
+    // }
+
+    // public List<Campo> findAll() {
+    //     return repository.findAll();
+    // }
+
+    // public Campo insert(Campo obj) {
+    //     return repository.save(obj);
+    // }
+
+    // public Campo update(Campo obj) {
+    //     findById(obj.getId());
+    //     return repository.save(obj);
+    // }
+
+    // public void delete(Integer id) {
+    //     findById(id);
+    //     repository.deleteById(id);
+    // }
+
     @Autowired
     private CampoRepository repository;
 
-    public Campo findById(Integer id) {
-        return repository.findById(id).get();
+    public Campo findById(final Integer id) {
+        try {
+            Campo obj = repository.findById(id).get();
+            return obj;
+        } catch (NoSuchElementException e) {
+            throw new les.ifoot.services.exceptions.ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Campo.class.getName());
+        }
     }
 
-    public List<Campo> findAll() {
+    public Collection<Campo> findAll() {
         return repository.findAll();
     }
 
-    public Campo insert(Campo obj) {
-        return repository.save(obj);
+    public Campo insert(final Campo obj) {
+        obj.setId(null);
+        try {
+            return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Campo não foi(foram) preenchido(s)");
+        }
     }
 
-    public Campo update(Campo obj) {
+    public Campo update(final Campo obj) {
         findById(obj.getId());
-        return repository.save(obj);
+        try {
+            return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Campo não foi(foram) preenchido(s)");
+        }
     }
 
-    public void delete(Integer id) {
+    public void delete(final Integer id) {
         findById(id);
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (final DataIntegrityViolationException e) {
+            throw new DataIntegrityException(
+                    "Não é possível excluir este Campo pois ele está cadastrado em algum espaço!");
+        }
     }
 }
